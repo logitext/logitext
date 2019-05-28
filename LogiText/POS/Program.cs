bbc50953c931b1b72826dd3a1d553ce88b0a9c59
Josh <jo3f@mtmail.mtsu.edu>
2019-05-27 21:49:39 -0500

    New Solution

---
+++ LogiText/POS/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POS
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            string connStr = Helper.ConnVal("TestDB");
+
+            LogiText.Data.DBManager dB = new LogiText.Data.DBManager(connStr);
+            dB.CreateTable("TableNew3", "col1 int");
+        }
+    }
+}
